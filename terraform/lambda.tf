c531874b81715b2e177ab74d252ea7fd12b3d42b
Frank Rosner <frank@fam-rosner.de>
2018-06-27 11:53:04 +0200

    Add logging and topic subscription

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -47,3 +47,45 @@ resource "aws_iam_role" "lambda_exec" {
 }
 EOF
 }
+
+resource "aws_sns_topic_subscription" "lambda" {
+  topic_arn = "${aws_sns_topic.upload.arn}"
+  protocol  = "lambda"
+  endpoint  = "${aws_lambda_function.slack.arn}"
+}
+
+resource "aws_lambda_permission" "sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.slack.function_name}"
+  principal     = "sns.amazonaws.com"
+  source_arn = "${aws_sns_topic.upload.arn}"
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name = "sns-sqs-lambda-logging"
+  path = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role = "${aws_iam_role.lambda_exec.name}"
+  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+}
